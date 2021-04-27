08e47c61bebf63850716144db4b968db7163fd23
Vageir <kantor.mark@hotmail.com>
2021-04-27 14:56:33 +0200

    CartForm

--- NetCincer/CartForm.Designer.cs
+++ NetCincer/CartForm.Designer.cs
@@ -29,12 +29,55 @@ namespace NetCincer
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.orderButton = new System.Windows.Forms.Button();
+            this.deleteCartLine = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // orderButton
+            // 
+            this.orderButton.Location = new System.Drawing.Point(12, 12);
+            this.orderButton.Name = "orderButton";
+            this.orderButton.Size = new System.Drawing.Size(75, 23);
+            this.orderButton.TabIndex = 1;
+            this.orderButton.Text = "Rendelés";
+            this.orderButton.UseVisualStyleBackColor = true;
+            // 
+            // deleteCartLine
+            // 
+            this.deleteCartLine.Location = new System.Drawing.Point(104, 11);
+            this.deleteCartLine.Name = "deleteCartLine";
+            this.deleteCartLine.Size = new System.Drawing.Size(75, 23);
+            this.deleteCartLine.TabIndex = 2;
+            this.deleteCartLine.Text = "Törlés";
+            this.deleteCartLine.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(200, 11);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // CartForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.deleteCartLine);
+            this.Controls.Add(this.orderButton);
+            this.Name = "CartForm";
             this.Text = "CartForm";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+        private System.Windows.Forms.Button orderButton;
+        private System.Windows.Forms.Button deleteCartLine;
+        private System.Windows.Forms.Button button2;
     }
 }
--- NetCincer/CartForm.cs
+++ NetCincer/CartForm.cs
@@ -10,9 +10,38 @@ namespace NetCincer
 {
     public partial class CartForm : Form
     {
-        public CartForm()
+        private ListView CartListView = new ListView();
+        Customer Customer;
+        String RestaurantID;
+        public CartForm(Customer customer, String restaurantID)
         {
             InitializeComponent();
+            this.Customer = customer;
+            this.RestaurantID = restaurantID;
+            InitListView();
         }
+        private void InitListView()
+        {
+            CartListView.Bounds = new Rectangle(new Point(12, 35), new Size(1085, 445));
+            CartListView.View = View.Details;
+            CartListView.LabelEdit = false;
+            CartListView.AllowColumnReorder = true;
+            CartListView.CheckBoxes = false;
+            CartListView.Columns.Add("Étel", 100, HorizontalAlignment.Left);
+            CartListView.Columns.Add("Mennyiség", 100, HorizontalAlignment.Right);
+            CartListView.Columns.Add("Ár", 100, HorizontalAlignment.Right);
+            int index = 0;
+            foreach (var item in Customer.Cart.GetFoods())
+            {
+                ListViewItem item1 = new ListViewItem(item.Key.Name, index);
+                item1.SubItems.Add(item.Value.ToString());
+                int price = item.Value * item.Key.Price;
+                item1.SubItems.Add(price.ToString());
+                CartListView.Items.Add(item1);
+                index++;
+            }
+            this.Controls.Add(CartListView);
+        }
+      
     }
 }
