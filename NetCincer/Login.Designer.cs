5af9404df38314ad1a1c19b4dc5e7b783e88be7b
Vageir <kantor.mark@hotmail.com>
2021-04-27 12:37:11 +0200

    cart

--- NetCincer/Login.Designer.cs
+++ NetCincer/Login.Designer.cs
@@ -54,6 +54,7 @@ namespace NetCincer
             this.lUsernameTextBox.Name = "lUsernameTextBox";
             this.lUsernameTextBox.Size = new System.Drawing.Size(413, 23);
             this.lUsernameTextBox.TabIndex = 1;
+            this.lUsernameTextBox.Text = "Joseph";
             // 
             // label2
             // 
@@ -71,6 +72,7 @@ namespace NetCincer
             this.lPasswordTextBox.Name = "lPasswordTextBox";
             this.lPasswordTextBox.Size = new System.Drawing.Size(413, 23);
             this.lPasswordTextBox.TabIndex = 3;
+            this.lPasswordTextBox.Text = "ohno";
             // 
             // lRegistrationButton
             // 
